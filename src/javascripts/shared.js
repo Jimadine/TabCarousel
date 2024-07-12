a62d2ea0d808ddbedbd99bf846da5a1c127ff5b0
Jim Adamson <jim.adamson@york.ac.uk>
2024-07-12 10:52:21 +0100

    shared.js: re-order "constants" in line with how the "defaults" are ordered in the same file

--- src/javascripts/shared.js
+++ src/javascripts/shared.js
@@ -1,23 +1,23 @@
 /**
  * shared.js
- * 
+ *
  * This script is responsible for managing shared constants, default values, and local storage operations.
- * 
+ *
  * @module
- * 
+ *
  * @exports {Object} LS - An object with methods for interacting with chrome's local storage.
  * @exports {Object} defaults - An object containing default values for the extension's settings.
  * @exports {Object} constants - An object containing constant keys used in the extension.
- * 
+ *
  * @property {Object} LS.getAllItems - Returns all items in chrome's local storage.
  * @property {Function} LS.getItem - Returns a specific item from chrome's local storage.
  * @property {Function} LS.setItem - Sets a specific item in chrome's local storage.
  * @property {Function} LS.removeItems - Removes specific items from chrome's local storage.
- * 
+ *
  * @property {number} defaults.flipWait_ms - The default time to wait before flipping to the next tab.
  * @property {number} defaults.reloadWait_ms - The default time to wait before reloading a tab.
  * @property {boolean} defaults.automaticStart - The default setting for whether the extension should start automatically.
- * 
+ *
  * @property {string} constants.reloadWait_ms - The key for the reload wait time setting.
  * @property {string} constants.flipWait_ms - The key for the flip wait time setting.
  * @property {string} constants.automaticStart - The key for the automatic start setting.
@@ -39,8 +39,8 @@ export const defaults = {
 };
 
 export const constants = {
-    reloadWait_ms: 'reloadWait_ms',
     flipWait_ms: 'flipWait_ms',
+    reloadWait_ms: 'reloadWait_ms',
     automaticStart: 'automaticStart',
     bypassCache: 'bypassCache',
     firstRun: 'firstRun'
